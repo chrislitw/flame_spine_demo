c2f06a6001d31249f0e44c06ff0789818ff29b7b
Chris Li <chrisli.dev@gmail.com>
2024-06-05 16:51:03 +0800

    fix

--- lib/game/flame_alpha_game.dart
+++ lib/game/flame_alpha_game.dart
@@ -20,6 +20,8 @@ class FlameAlphaGame extends FlameGame with KeyboardEvents {
     _spineBoy = SpineBoy();
     add(_spineBoy!);
 
+    createText();
+
     add(FpsTextComponent(position: Vector2(20, 20), scale: Vector2(0.8, 0.8)));
 
     super.onLoad();
@@ -31,5 +33,19 @@ class FlameAlphaGame extends FlameGame with KeyboardEvents {
     Flame.images.clearCache();
     Flame.assets.clearCache();
     super.onRemove();
+  }
+
+  void createText() {
+    TextComponent textChinese = TextComponent(
+      text: '測試中文',
+        position: Vector2(20, 40)
+    );
+    add(textChinese);
+
+    TextComponent text = TextComponent(
+        text: 'test text',
+        position: Vector2(20, 80)
+    );
+    add(text);
   }
 }
